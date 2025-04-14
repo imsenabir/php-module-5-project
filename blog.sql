130d6b14515c6075d33b83a54d72cdcd7a5d9ba9
Imsen Abir <000emonali01@gmail.com>
2025-04-14 10:54:49 +0600

    Add files via upload

---
+++ blog.sql
@@ -0,0 +1,65 @@
+
+-- This SQL script creates a database and tables for a simple blog application.
+CREATE DATABASE blog ;
+
+-- Use the created database
+USE blog ;
+
+ -- creating users table
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- creating posts table
+CREATE TABLE posts(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- creating comments table
+CREATE TABLE comments(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    content TEXT NOT NULL,
+
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- iserting data into users table
+INSERT INTO users(email,password) VALUE ('lazy-panda@project.com','@!PANDA!@') ;
+
+-- inserting data into posts table
+INSERT INTO posts(title,content,user_id) VALUE ('My first Panda post','This is the content of my first Panda post.','1') ;
+
+-- inserting data into comments table
+INSERT INTO comments(content,post_id,user_id) VALUE ('This is a comment on the first post.','1','1') ;
+
+--getting all post with user info
+SELECT u.id AS user_id, u.email AS user_email,
+       p.id AS post_id, p.title AS post_title,
+       c.id AS comment_id, c.comment AS comment
+
+FROM users AS u 
+
+JOIN posts AS p ON u.id = p.user_id
+
+JOIN comments AS c ON p.id = c.post_id
+WHERE c.user_id = u.id
+
+
+
